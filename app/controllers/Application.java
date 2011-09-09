ca2623ced6edcf3c410cb755f4589f0278f3a21a
Géraud <geraudster@gmail.com>
2011-09-09 22:23:04 +0200

    first GUI, CSS

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,9 +8,16 @@ import java.util.*;
 import models.*;
 
 public class Application extends Controller {
+    @Before
+    static void addDefaults() {
+        renderArgs.put("trombineTitle", Play.configuration.getProperty("trombine.title"));
+        renderArgs.put("trombineBaseline", Play.configuration.getProperty("trombine.baseline"));
+    }
 
     public static void index() {
-        render();
+        List<Person> persons = Person.find("order by organization.name asc, lastName asc, firstName asc").fetch(10);
+        //        List<Person> persons = Person.findAll();
+        render(persons);
     }
 
 }
